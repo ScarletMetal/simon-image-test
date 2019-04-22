5abe6bbcf4a0046dd57d2612843309b5cca7d0c7
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-04-22 14:31:23 +0300

    imported material theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { UploadButtonComponent } from "./upload-button/upload-button.component";
+import { DisplayImageComponent } from "./display-image/display-image.component";
+
+import {MatButtonModule} from "@angular/material";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadButtonComponent,
+    DisplayImageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
