f7385c309080964dab0abe7a65e40ff68b266043
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-04-22 19:22:52 +0300

    added filetype validation to select-button

--- src/app/modal/select-button/select-button.component.ts
+++ src/app/modal/select-button/select-button.component.ts
@@ -1,6 +1,10 @@
 import {Component, EventEmitter, OnInit, Output, ViewChild} from "@angular/core";
 import {Image} from "../../image";
 
+const ImageFileTypes = [
+  "jpg", "jpeg", "png"
+];
+
 @Component({
   selector: "app-select-button",
   templateUrl: "./select-button.component.html",
@@ -16,19 +20,30 @@ export class SelectButtonComponent implements OnInit {
   @ViewChild("file") file;
 
   @Output() imageEvent = new EventEmitter<Image>();
-
+  @Output() errorEvent = new EventEmitter<string>();
   click() {
     this.file.nativeElement.click();
+  }
+
+  validateFileName(name: string) {
+    const items = name.split(".");
+    const extension = items[items.length - 1];
+    return extension in ImageFileTypes;
   }
 
   onFileSelected(event: any) {
     if (event.target.files && event.target.files[0]) {
       const reader = new FileReader();
       const file = event.target.files[0];
-      reader.onload = (readingEvent: any) => {
-        const img: Image = {content: readingEvent.target.result, name: file.name};
-        this.imageEvent.emit(img);
-      };
+
+      if (this.validateFileName(file.name)) {
+        reader.onload = (readingEvent: any) => {
+          const img: Image = {content: readingEvent.target.result, name: file.name};
+          this.imageEvent.emit(img);
+        };
+      } else {
+        this.errorEvent.emit("Invalid File Type");
+      }
 
       reader.readAsDataURL(file);
     }
