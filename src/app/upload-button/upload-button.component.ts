6883160ad1efd09c6a79da22bcd4431888838801
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-04-22 17:31:11 +0300

    made upload-button.component work with Image

--- src/app/upload-button/upload-button.component.ts
+++ src/app/upload-button/upload-button.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ViewChild} from "@angular/core";
 import {MatButtonModule} from "@angular/material";
 import {ImageService} from "../image.service";
+import {Image} from "../image";
 
 @Component({
   selector: "app-upload-button",
@@ -12,7 +13,6 @@ export class UploadButtonComponent implements OnInit {
   constructor(private imageService: ImageService) {
   }
 
-  url = "";
   @ViewChild("file") file;
 
   click() {
@@ -25,8 +25,8 @@ export class UploadButtonComponent implements OnInit {
       const reader = new FileReader();
       const file = event.target.files[0];
       reader.onload = (readingEvent: any) => {
-        this.imageService.setImage(readingEvent.target.result);
-        this.imageService.setName(file.name);
+        const img: Image = {content: readingEvent.target.result, name: file.name};
+        this.imageService.setImage(img);
       };
 
       reader.readAsDataURL(file);
