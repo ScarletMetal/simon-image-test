f4ee5eca5f1880209d8567df47cd42eefd295f5e
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-04-22 18:40:16 +0300

    added all imports to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,35 @@ import { AppComponent } from "./app.component";
 import { SelectButtonComponent } from "./select-button/select-button.component";
 import { DisplayImageComponent } from "./display-image/display-image.component";
 
-import {MatButtonModule, MatDialogModule} from "@angular/material";
+import {MatButtonModule, MatDialogModule, MatFormFieldModule, MatInputModule} from "@angular/material";
+import { DialogModalComponent } from "./modal/dialog-modal/dialog-modal.component";
+import { UploadButtonComponent } from "./modal/upload-button/upload-button.component";
+
+import { FormsModule } from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SelectButtonComponent,
-    DisplayImageComponent
+    DisplayImageComponent,
+    DialogModalComponent,
+    UploadButtonComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
     MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    BrowserAnimationsModule
+  ],
+  entryComponents: [
+    DialogModalComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
